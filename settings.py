279c3020d6bad3537b9cf4853027bde2bf85724d
dev-rome <haynesjerome@gmail.com>
2022-06-28 08:57:05 -0400

    added settings for ship to have speed

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 class Settings():
-   # Class to store all settings for Alien Invasion.
-   def __init__(self):
-    # Initialize the game's settings.
-    # Screen settings.
-    self.screen_width = 1200
-    self.screen_height = 800
-    self.bg_color = (230, 230, 230)
+    # Class to store all settings for Alien Invasion.
+    def __init__(self):
+        # Initialize the game's settings.
+        # Screen settings.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        # Ship speed.
+        self.ship_speed_factor = 1.5
