89185efe5844b303362839430ce93c0f2ec377f2
dev-rome <haynesjerome@gmail.com>
2022-06-28 15:53:49 -0400

    set flag state for game active to true

--- game_stats.py
+++ game_stats.py
@@ -2,6 +2,8 @@ class GameStats():
     def __init__(self, ai_settings):
         self.ai_settings = ai_settings
         self.reset_stats()
+        # Start Alien Invasion in an active state.
+        self.game_active = True
 
     def reset_stats(self):
         self.ship_left = self.ai_settings.ship_limit    
