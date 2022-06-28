d70f1b94b680a94c853f33753c0e1a801296d0e0
dev-rome <haynesjerome@gmail.com>
2022-06-28 15:31:06 -0400

    removed spaces

--- alien.py
+++ alien.py
@@ -8,18 +8,15 @@ class Alien(Sprite):
         super(Alien, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
-
         # Load the alien image and set its rect.
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
-
         # Start each new alien near the top left of the screen.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
-
         # Store exact position.
         self.x = float(self.rect.x)
-
+        
     def check_edges(self):
         screen_rect = self.screen.get_rect()
         if self.rect.right >= screen_rect.right:
