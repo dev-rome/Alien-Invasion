0dbc13a03a1fad45780dfca8f63a8bc1804ee611
dev-rome <haynesjerome@gmail.com>
2022-06-28 14:45:50 -0400

    new fleet appears when current fleet is destroy

--- game_functions.py
+++ game_functions.py
@@ -31,14 +31,17 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(aliens, bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     bullets.update()
     # Delete bullets that have disappeared.
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
-
+    if len(aliens) == 0:
+        # Destroy existing bullets and create new fleet.
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     # Create a new bullet and add it to the bullets group.
