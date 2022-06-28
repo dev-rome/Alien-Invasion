8cb7a7ca7741700b2009e333e06e1b8e89792d71
dev-rome <haynesjerome@gmail.com>
2022-06-28 09:02:20 -0400

    added settings for ship bullets

--- settings.py
+++ settings.py
@@ -8,3 +8,9 @@ class Settings():
         self.bg_color = (230, 230, 230)
         # Ship speed.
         self.ship_speed_factor = 1.5
+        # Ship bullet
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3 
