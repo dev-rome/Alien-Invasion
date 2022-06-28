eaba805813ce6c8b263e0d014d808583476aa834
dev-rome <haynesjerome@gmail.com>
2022-06-28 15:36:58 -0400

    function to center ship

--- ship.py
+++ ship.py
@@ -12,13 +12,11 @@ class Ship():
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-
         # Start each new ship at the bottom center of screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
         # Store a decimal value for the ship's center.
         self.center = float(self.rect.centerx)
-
         self.moving_right = False
         self.moving_left = False
 
@@ -29,9 +27,11 @@ class Ship():
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
-
         self.rect.centerx = self.center    
 
     def blitme(self):
         # Draw ship at current location.
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.center = self.screen_rect.centerx    
