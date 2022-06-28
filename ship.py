4245eca02bc4b51045091566521d467a9525e502
dev-rome <haynesjerome@gmail.com>
2022-06-28 08:56:05 -0400

    set object attributes for ship to have movement

--- ship.py
+++ ship.py
@@ -4,10 +4,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         # Initialize the ship and set its starting position.
         self.screen = screen
-
+        self.ai_settings = ai_settings
         # Load ship image.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -16,6 +16,21 @@ class Ship():
         # Start each new ship at the bottom center of screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # Update ship position based on if flag is true or false.
+        # Update the ship's center value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center    
 
     def blitme(self):
         # Draw ship at current location.
