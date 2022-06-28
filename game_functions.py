024932c1e7910711eab249893d098913d3143ec9
dev-rome <haynesjerome@gmail.com>
2022-06-28 14:23:33 -0400

    functions to check if edges was hit if so fleet direction will change

--- game_functions.py
+++ game_functions.py
@@ -101,5 +101,17 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def update_aliens(aliens):
+def check_fleet_edges(ai_settings, aliens):
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1    
+
+def update_aliens(ai_settings, aliens):
+    check_fleet_edges(ai_settings, aliens)
     aliens.update()
