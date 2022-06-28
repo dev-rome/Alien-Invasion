2b9b78dbcb559b8e655424218995fd75b6031c7b
dev-rome <haynesjerome@gmail.com>
2022-06-28 10:11:32 -0400

    created fire bullet and update bullet functions

--- game_functions.py
+++ game_functions.py
@@ -29,13 +29,26 @@ def update_screen(ai_settings, screen, ship, bullets):
     pygame.display.flip()
 
 
+def update_bullets(bullets):
+    bullets.update()
+    # Delete bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group.
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet) 
+        fire_bullet(ai_settings, screen, ship, bullets)
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
 
