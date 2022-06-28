f1b92a0f75aa4f195e470dea8434d64748564994
dev-rome <haynesjerome@gmail.com>
2022-06-28 08:54:11 -0400

    added ship to gf.check_events and ai_settings to ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,13 +13,14 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Ship
-    ship = Ship(screen)
+    # Ship.
+    ship = Ship(ai_settings, screen)
 
     # Main loop for game.
     while True:
         # Watch for keyboard and mouse events.
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
